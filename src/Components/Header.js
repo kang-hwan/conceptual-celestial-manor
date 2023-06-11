51c77fd7cd209bc3d65ee8d71d732e0fef7a37aa
Roy Kanghwan Kim <roykim0416@gmail.com>
2023-06-11 20:59:55 +1000

    mobile navbar start

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../scss/App.css";
 import { Link } from "react-router-dom";
+import headerLogoMobile from "../Images/header-logo-mobile.svg";
 
 export default function Header() {
   return (
     <header>
-      <div className="header-wrapper">
+      <div className="header-wrapper--desktop">
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -29,6 +30,20 @@ export default function Header() {
         <a href="/" className="nav-booking">
           BOOK NOW
         </a>
+      </div>
+      <div className="header-wrapper--mobile">
+        <div className="header-wrapper__">
+          <img src={headerLogoMobile} alt="header-logo-mobile" />
+        </div>
+        <div className="header-wrapper--mobile__navMenu">
+          <a>BOOK NOW</a>
+          <div className="hamburgerContainer">
+            <button className="hamburger">
+              <span className="hamburgerBar"></span>
+              <span className="hamburgerBar"></span>
+            </button>
+          </div>
+        </div>
       </div>
     </header>
   );
